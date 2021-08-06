fddb60678998e29ab9301ef7954c82b666b105c2
urmil89 <urmil8989@gmail.com>
2021-08-06 23:30:39 +0530

    public style.css

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
 
-const port = 3000;
 // express app
 
 const app = express();
@@ -9,7 +9,25 @@ const app = express();
 app.set("views engine", "ejs");
 
 // listen for request
-app.listen(port);
+app.listen(3000);
+
+// middleware & static files
+
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  console.log("new request made");
+  console.log("host:", req.hostname);
+  console.log("path:", req.path);
+  console.log("method:", req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in the next middleware");
+  next();
+});
 
 app.get("/", (req, res) => {
   const blogs = [
@@ -17,20 +35,19 @@ app.get("/", (req, res) => {
     // {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     // {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
   ];
-  res.render('index.ejs', { title: 'Home', blogs });
+  res.render("index.ejs", { title: "Home", blogs });
 });
 app.get("/about", (req, res) => {
-  res.render("about.ejs",{title:'About'});
+  res.render("about.ejs", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create.ejs',{title:'Create a new Blog'});
+app.get("/blogs/create", (req, res) => {
+  res.render("create.ejs", { title: "Create a new Blog" });
 });
 
 // 404 page
 
 app.use((req, res) => {
   // res.sendFile("./views/404.html", { root: __dirname });
-  res.status(404).render("404.ejs",{title:'404'});
+  res.status(404).render("404.ejs", { title: "404" });
 });
-
